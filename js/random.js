3858d316d31dde57763b7eeb85ca5f98d5695003
uncle <15210834207@163.com>
2019-01-30 10:05:41 +0800

    tupian

--- js/random.js
+++ js/random.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 function choosePic() {
-    var myPix = new Array("img/service1.jpg","img/service2.jpg","img/follow.jpg");
+    var myPix = new Array("img/service1.jpg","img/service2.jpg","img/service3.jpg","img/service4.jpg","img/service5.jpg","img/follow.jpg");
     var randomNum = Math.floor((Math.random() * myPix.length));
     document.getElementById("myPicture").src = myPix[randomNum];
 }
